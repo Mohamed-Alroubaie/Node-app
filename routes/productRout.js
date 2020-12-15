a94dda1b3277494ed8882cb2ebdbd8a796d7921b
Mohamed-Alroubaie <65648087+Mohamed-Alroubaie@users.noreply.github.com>
2020-12-15 02:39:45 +0100

    typo

--- routes/productRout.js
+++ routes/productRout.js
@@ -3,7 +3,6 @@ import productController from '../controllers/productController.js';
 
 const productRoute = Router();
 
-productRoute.get('/api/products', productController);
-productRoute.post('/api/products', productController);
+productRoute.use('/api/products', productController);
 
 export default productRoute;
